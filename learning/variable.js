95715ddae0973b722d1cb51d26dd3bfb35080583
Prathamm-saini <prathamzxc@gmail.com>
2025-07-13 13:20:48 +0530

    Add initial project files: launch configuration, HTML structure, and variable declarations

---
+++ learning/variable.js
@@ -0,0 +1,14 @@
+// print statement in js
+console.log("javascript")
+
+//declaring constants in js
+// once declared cannot be changed any further
+
+const accountId = 144553
+
+// different ways to declare variables in js 
+
+let accountEmail ="prathamzxc@gmail.com"
+var  accountPassword ="12345"
+accountCity = "New  Delhi"
+
